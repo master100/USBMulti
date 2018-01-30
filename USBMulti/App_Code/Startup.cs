f9a1396b1c1e3b9747a1f45e28b3ab5c816e0725
Roberto Regulo <master100@gmail.com>
2018-01-30 09:33:54 -0200

    primeiro commit local

---
+++ USBMulti/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(USBMulti.Startup))]
+namespace USBMulti
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
